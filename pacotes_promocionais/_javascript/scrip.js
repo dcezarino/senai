21a2e87f5b96661a39f421324638dcb080cf438d
Diego Cezarino <cezarino.diego@gmail.com>
2021-08-09 21:50:12 -0300

    Criando projeto pacotes viagens

---
+++ pacotes_promocionais/_javascript/scrip.js
@@ -0,0 +1,8 @@
+function abrirModal() {
+    document.getElementById('modal').style.top = "0";
+    document.getElementById('modal').style.display = "block";
+}
+function fecharModal() {
+    document.getElementById('modal').style.top = "-100%";
+    document.getElementById('modal').style.display = "none";
+}
