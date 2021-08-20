390ed61234cfe3a30546b0ed2959d9c849153e92
Diego Cezarino <cezarino.diego@gmail.com>
2021-08-19 22:43:29 -0300

    Atividade verificação pré cadastro

---
+++ esporte/_javascript/tools.js
@@ -0,0 +1,12 @@
+// Função para abrir o novo formulário
+function abrir() {
+    var mensagem = "Clique aqui";
+    document.getElementById("linkform").innerHTML=mensagem;
+}
+
+// Função para fechar a janela
+function fechar() {
+    alert("Apenas maiores de 18 anos poderão participar das promoções");
+    window.close();
+}
+
