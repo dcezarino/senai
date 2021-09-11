722d85a3f95eb9bfc905de1d98da02b8f418b867
Diego Cezarino <cezarino.diego@gmail.com>
2021-09-10 22:05:14 -0300

    Inserindo validações nos campos do cadastro

--- projeto/_javascript/jquery_funcoes.js
+++ projeto/_javascript/jquery_funcoes.js
@@ -1,30 +1,28 @@
-﻿
-//Construção do efeito ACCORDION (ACORDEON) do JQUERY UI
-  $(function() {
-    $( "#acordeon" ).accordion({
-        active: false, 
-        collapsible: true
-    });
+﻿//Construção do efeito ACCORDION (ACORDEON) do JQUERY UI
+$(function () {
+  $("#acordeon").accordion({
+    active: false,
+    collapsible: true
+  });
 
 
   //VALIDAÇÃO JQUERY IGUAL DO MATERIAL SOMENTE PARA O NOME
-  $( "#form" ).validate({   	debug: true,
-	rules: {
-	   nome: {        required: true,        minlength: 10    }		
-	},
-	messages: {
-	    nome: {
-        	required: "<li>Por favor entre com seu nome.</li>",
-	        minlength: "<li>Seu nome deve ser maior.</li>"
-    		}
-	 // , email: "<li>Por favor entre com seu email</li>"
-		
-	}
-    }); // fechamento do validate
+  $("#form").validate({
+    debug: true,
+    rules: {
+      nome: { required: true, minlength: 10 }
+    },
+    messages: {
+      nome: {
+        required: "<li>Por favor entre com seu nome.</li>",
+        minlength: "<li>Seu nome deve ser maior.</li>"
+      }
+      // , email: "<li>Por favor entre com seu email</li>"
 
+    }
+  }); // fechamento do validate
 
-
-//VALIDAÇÃO COM JQUERY
+  //VALIDAÇÃO COM JQUERY
 
   // a  Representa caracteres alfabéticos (letras) (A-Z,a-z)
   // a  Porém 'a' não permite acentuação nem ç 
@@ -32,15 +30,17 @@
   // *  Representa caracteres alfanuméricos (letras e números) (A-Z,a-z,0-9)
   // Uso do ? transforma o caracter seguinte opcional
 
-   //Mascara para CPF com o .class
-   $('.mask-cpf').mask('999.999.999-99'); 
+  //Mascara para CPF com o .class
+  $('.mask-cpf').mask('999.999.999-99');
 
-   //Mascara para o Celular com o seletor ID, 
-   // recebe 8 ou 9 digitos
-   $("#cel").mask("(99)9999-9999?9");
+  //Mascara para o Celular com o seletor ID, 
+  // recebe 8 ou 9 digitos
+  $("#cel").mask("(99)9999-9999?9");
 
-   //Mascara para Salario - valores monetários
-   $('#salario').maskMoney({showSymbol:true,symbol:'R$ ', 
-     decimal:',', thousands:'.', allowZero:true}); 
-
+  //Mascara para Salario - valores monetários
+  $('#salario').maskMoney({
+    showSymbol: true, symbol: 'R$ ',
+    decimal: ',', thousands: '.', allowZero: true
   });
+
+});
