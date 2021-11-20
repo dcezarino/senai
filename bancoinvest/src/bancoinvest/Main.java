14158783d4edaa21a538b3861f7c431b5988d153
Diego Cezarino <cezarino.diego@gmail.com>
2021-11-20 16:20:40 -0300

    Inserindo mensagem informativa

--- bancoinvest/src/bancoinvest/Main.java
+++ bancoinvest/src/bancoinvest/Main.java
@@ -4,8 +4,10 @@ public class Main {
 
 	public static void main(String[] args) {
 		
+		// Criando uma nova conta
 		ContaCorrente contaCliente = new ContaCorrente(23456789, 100, 34567, 50, 1, "20/11/2021");
-		// Criando um novo cliente e uma nova conta para o mesmo
+		
+		// Criando um novo cliente 
 		Cliente cliente = new Cliente(100, 34567, "123.456.788-90", "José da Silva", "15/06/1995", "Av. dos Autonomistas, 234", "(11)9123-456", 100, "abc#1234", contaCliente);
 				
 		// Imprimindo os dados do cliente 
