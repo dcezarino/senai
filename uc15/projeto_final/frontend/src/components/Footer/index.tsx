bf3c0029fedc7526d43b18b13f9c11ef191d9a7a
Diego Cezarino <cezarino.diego@gmail.com>
2022-04-14 23:39:42 -0300

    Adicionando projeto frontend React

---
+++ uc15/projeto_final/frontend/src/components/Footer/index.tsx
@@ -0,0 +1,12 @@
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/dcezarino" target="_blank" rel="noreferrer">Diego Cezarino</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+                    Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+            </div>
+        </footer>);
+}
+
+export default Footer;
